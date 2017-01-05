4d61951d53573356495c177eb72f6335267a593c
Chris Fowke <Cfowke@Chriss-MacBook-Pro.local>
2017-01-05 10:46:17 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// your code, here
+automatDishes = [
+  'Chicken Pot Pie',
+  'Baked Beans',
+  'Macaroni and Cheese',
+  'Burgundy Sauce with Beef and Noodles',
+  'Creamed Spinach',
+  'Pumpkin Pie',
+  'Strawberry Shortcake'];
+
+let automatItemPrint = () => {
+  automatDishes.forEach((item) => {
+    console.log(item);
+  });
+}
+
+let mainDiv = document.getElementById('main')
+mainDiv.innerHTML = automatItemPrint
